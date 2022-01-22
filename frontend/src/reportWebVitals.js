4f9351083064af0bda52a051e39d08786c2dceba
Phill Conrad <phtcon@ucsb.edu>
2022-01-21 17:30:35 -0800

    pc - remove react-bootstrap-table-next and document reportWebVitals

--- frontend/src/reportWebVitals.js
+++ frontend/src/reportWebVitals.js
@@ -1,3 +1,5 @@
+// https://create-react-app.dev/docs/measuring-performance/
+
 const reportWebVitals = onPerfEntry => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
     import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
